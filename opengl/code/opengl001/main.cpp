ccfb6fe069f3dcc568ed918f03117b0b96ddc985
zen <869158130@qq.com>
2023-08-04 21:31:11 +0800

    opengl

--- opengl/code/opengl001/main.cpp
+++ opengl/code/opengl001/main.cpp
@@ -13,28 +13,43 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
+    //调用glfwInit函数来初始化GLFW
     glfwInit();
+    //使用glfwWindowHint函数来配置GLFW
+    //将主版本号(Major)和次版本号(Minor)都设为3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //告诉GLFW我们使用的是核心模式(Core-profile)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Mac OS X系统需要加入下面的代码
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    //创建一个窗口对象，这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
     if (window == nullptr) {
         std::cout << "failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+    //通知GLFW将我们窗口的上下文设置为当前线程的主上下文了
     glfwMakeContextCurrent(window);
+
+    //GLAD是用来管理OpenGL的函数指针的，所以在调用任何OpenGL的函数之前我们需要初始化GLAD
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "failed to initialize GLAD" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    //必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。
+    // 我们可以通过调用glViewport函数来设置窗口的维度(Dimension)
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    //当用户改变窗口的大小的时候，视口也应该被调整
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     init();
+    //渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行
     while (!glfwWindowShouldClose(window)) {
         on_input(window);
         draw_frame();
@@ -42,6 +57,7 @@ int main()
         glfwPollEvents();
     }
 
+    //正确释放/删除之前的分配的所有资源
     glfwTerminate();
     return 0;
 }
@@ -51,15 +67,20 @@ void init()
 
 }
 
+//渲染
 void draw_frame()
 {
+    //glClearColor来设置清空屏幕所用的颜色。
+    // 当调用glClear函数，清除颜色缓冲之后，整个颜色缓冲都会被填充为glClearColor里所设置的颜色
     glClearColor(.2f, .3f, .3f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void on_input(GLFWwindow* window)
 {
+    //检查用户是否按下了返回键(Esc)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        //关闭GLFW
         glfwSetWindowShouldClose(window, true);
     }
 }
