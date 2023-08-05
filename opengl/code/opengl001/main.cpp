ad95745f82305dfc83bacb59b24655a805b4795a
zen <869158130@qq.com>
2023-08-05 11:11:35 +0800

    窗口

--- opengl/code/opengl001/main.cpp
+++ opengl/code/opengl001/main.cpp
@@ -2,14 +2,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define WINDOW_TITLE "gl-playground"
+// 设置
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-void init();
 void draw_frame();
-void on_input(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 
 int main()
 {
@@ -21,11 +21,14 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     //告诉GLFW我们使用的是核心模式(Core-profile)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
     //Mac OS X系统需要加入下面的代码
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     //创建一个窗口对象，这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == nullptr) {
         std::cout << "failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -43,16 +46,17 @@ int main()
 
     //必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。
     // 我们可以通过调用glViewport函数来设置窗口的维度(Dimension)
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     //当用户改变窗口的大小的时候，视口也应该被调整
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    init();
     //渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行
     while (!glfwWindowShouldClose(window)) {
-        on_input(window);
+        processInput(window);
         draw_frame();
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -76,7 +80,7 @@ void draw_frame()
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void on_input(GLFWwindow* window)
+void processInput(GLFWwindow* window)
 {
     //检查用户是否按下了返回键(Esc)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
